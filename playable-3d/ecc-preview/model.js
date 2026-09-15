6858dff98ddaaa788ccc456bc7e2ba36555b34a8
taniab1975 <tania.byrnes@cewa.edu.au>
2026-09-15 10:49:57 +0800

    Remove Arrival courtyard benches

--- playable-3d/ecc-preview/model.js
+++ playable-3d/ecc-preview/model.js
@@ -1,4 +1,4 @@
-import {buildAuthoredCourtyard} from './authored-courtyard.js?v=courtyard-bench-and-pole-20260915';
+import {buildAuthoredCourtyard} from './authored-courtyard.js?v=courtyard-benches-removed-20260915';
 import * as T from 'three';
 import {addCourtyardArchitecture,addCourtyardPlanting} from './courtyard-detail.js?v=courtyard1';
 import {GLTFLoader} from 'three/addons/loaders/GLTFLoader.js';
